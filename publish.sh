6f061e90a8eb892861bedd60983cad0ecdba86d5
Naker90 <Antoniojesussg96@gmail.com>
2020-03-23 22:27:50 +0100

    improve publish script

--- publish.sh
+++ publish.sh
@@ -1,19 +1,66 @@
 #!/bin/bash
 
-echo "[+] Installing NPM dependencies..."
-npm i
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
 
-echo "[+] Clean old distribution folder..."
-rm -r dist &>/dev/null
+function exitIfLastCommandFail() {
+	if [ $? -eq 1 ]; then
+		echo -e "${RED}[ERR!] $1."
+		exit 1
+	fi
+}
 
-echo "[+] Compiling..."
-./node_modules/typescript/bin/tsc
+function clean() {
+	if [ -d dist ]; then
+		echo -e "${YELLOW}[INFO] Clean old distribution folder..."
+		rm -r dist 
+	fi
+	if [ -d node_modules ]; then
+		echo -e "${YELLOW}[INFO] Clean old instaled node modules..."
+		rm -r node_modules
+	fi
+}
 
-echo "[+] Move package.json to distribution folder and change main file to index.js"
-sed "s/src\/queryStringSerializer.ts/index.js/g" package.json > dist/package.json
+function installNpmDependencies() {
+	if [ ! -f package.json ]; then
+		echo -e "${RED}[ERR!] package.json must exist."
+		exit 1
+	fi
+	echo -e "${YELLOW}[INFO] Installing NPM dependencies..."
+	npm i
+	exitIfLastCommandFail "Npm dependencies instalation fails."
+}
 
-echo "[+] Move to distribution folder"
-cd dist
+function compile() {
+	if [ ! -f ./node_modules/typescript/bin/tsc ]; then
+		echo -e "${RED}[ERR!] TypeScript must be a dependency of the project."
+		exit 1
+	fi
+	echo -e "${YELLOW}[INFO] Compiling..."
+	./node_modules/typescript/bin/tsc
+	exitIfLastCommandFail "TypeScript compilation fails."
+}
 
-echo "[+] Publish to npm..."
-npm publish --dry-run
+function movePackageConfigFileToDistributionFolder(){
+	echo -e "${YELLOW}[INFO] Move package.json to distribution folder and change main file to index.js"
+	sed "s/src\/queryStringSerializer.ts/index.js/g" package.json > dist/package.json
+}
+
+function moveToDistributionFolder() {
+	echo -e "${YELLOW}[INFO] Move to distribution folder"
+	cd dist
+}
+
+function publishPackage() {
+	echo -e "${YELLOW}[INFO] Publish to npm..."
+	npm publish --dry}run
+	exitIfLastCommandFail "NPM package publication fails."
+}
+
+clean
+installNpmDependencies
+compile
+movePackageConfigFileToDistributionFolder
+moveToDistributionFolder
+publishPackage
